6e9aa50d96977d27e65fb7341e24fb6341118982
BCRSY <36918910+BCRSY@users.noreply.github.com>
2018-03-21 17:17:38 +0700

    Add files via upload

---
+++ Assets/camerarotation.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class camerarotation : MonoBehaviour {
+	public Quaternion rotation = Quaternion.Euler(0, 0, 0);
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () {
+			if(Input.GetKey(KeyCode.A))
+			{
+			transform.rotation = Quaternion.Euler(0, -30, 0);
+			}
+			if(Input.GetKey(KeyCode.D))
+			{
+			transform.rotation = Quaternion.Euler(0, 30, 0);
+			}
+	}
+}
